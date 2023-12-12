ad77c242d804ce05d94a7bec0e60cf1f16bbd7d7
Tito <totitron20@gmail.com>
2023-12-12 14:02:45 -0500

    Explicacion de cada punto del primer hola mundo y ejec

--- holamundo.c
+++ holamundo.c
@@ -1,5 +1,12 @@
+// esta linea de codigo se tiene que poner casi siempre
+// es una libreria para utilizar entradas y salidas estandar
+// se describira mas adelante
 #include <stdio.h>
 
+// int main debe ir siempre en todos los programas
+// es la funcion principal de los programas
 int main() {
+
+    // printf escribe en consola la cadena puesta
     printf("hola, mundo\n");
 }
