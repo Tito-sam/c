44c2d6b1c5c5abc88af4b815d5d763e1355b8007
Tito Samuel Mendez <totitron20@gmail.com>
2024-03-25 17:37:19 -0500

    Continuando parte funciones, y creacion ignore

--- Episodio 1/Funciones1.7.c
+++ Episodio 1/Funciones1.7.c
@@ -5,16 +5,33 @@
     por elemplo lo son el prinf() o el putchar(), que son funciones dadas 
     por la libreria estandar de c, que tienen una finalidad.
 */
+
+/*
+    Aqui se puede observar como se define una funcion, es como declarar una variable,
+    No tiene nada para hacer todavia, pero se crea con la siguiente forma:
+        Tipo-retorno NombreFuncion (Parametros si tiene);
+
+        Tipo-retorno hace referencia al tipo de dato que va a devolver, en el caso de 
+        power, es int ya que la funcion devolvera un dato numerico entero.
+
+        NombreFuncion hace referencia al nombre o identificador de la funcion.
+
+        Parametros hace referencia a los datos que necesita y se les dara para que 
+        pueda funcionar. 
+*/
 int power(int m, int n);
 
 int main() {
     int i;
 
     for(i = 0; i < 10; ++i) {
+        /*
+            En este caso, podemos ver como se utiliza power en la funcion principal.
+        */
         printf("%d %d %d\n", i , power(2,i), power(-3,i));
     }
     return 0;
-}
+};
 
 int power(int base, int n) {
     int i, p;
@@ -23,4 +40,4 @@ int power(int base, int n) {
         p = p * base;
         return p;
     }
-}
+};
